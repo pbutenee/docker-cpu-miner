9a2fa946c2c77cdecb4d742f57e6dcb8513eefbf
Pieter Buteneers <pieter.buteneers@coscale.com>
2018-01-28 15:26:53 +0100

    removed copy pasted code

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,7 @@ import multiprocessing
 import subprocess
 import json
 import logging
+import cpuminer_driver
 
 
 def run(nicehash_algorithms):
@@ -40,15 +41,7 @@ def run(nicehash_algorithms):
 	            output = subprocess.check_output(['bash', '-c', bash_command + ' -t ' + str(t)]).decode("utf-8")
 	            output = output[output.rfind(benchmark_str) + len(benchmark_str) : ]
 	            output = output[ : output.find('H/s')]
-	            hash_rate = float(output[ : output.rfind(' ')])
-	            if output[-1] == 'k':
-	                hash_rate *= 1000
-	            elif output[-1] == 'M':
-	                hash_rate *= 1000000
-	            elif output[-1] == 'G':
-	                hash_rate *= 1000000000
-	            elif output[-1] != ' ':
-	                raise(NotImplementedError('The following unit is not yet supported: ' + output[-1] + ' or there is something wrong with the output: ' + output))
+	            hash_rate = cpuminer_driver._convert_to_float(output)
 	            if hash_rate > optimal_hash_rate:
 	                optimal_hash_rate = hash_rate
 	                optimal_nof_threads = t
@@ -71,6 +64,5 @@ def run(nicehash_algorithms):
 
 
 if __name__ == '__main__':
-	import cpuminer_driver
 	paying, ports = cpuminer_driver.nicehash_multialgo_info()
 	run(paying.keys())
